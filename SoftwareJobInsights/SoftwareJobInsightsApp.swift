49eea4108c65d18cdc8c9f166285d8617b1b0233
Quinn Caverly <quinncaverly@gmail.com>
2024-12-03 13:06:57 -0500

    Initial Commit

---
+++ SoftwareJobInsights/SoftwareJobInsightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftwareJobInsightsApp.swift
+//  SoftwareJobInsights
+//
+//  Created by Caverly, Quinn on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SoftwareJobInsightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
